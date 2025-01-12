bc10d385da73681caef6c163ebce74c944b161fe
Patryk Zadykowicz <patrykzadykowiczali@gmail.com>
2025-01-12 21:41:34 +0100

    Przekazywanie składników do przepisów

--- MealPlanner/Controllers/RecipeController.cs
+++ MealPlanner/Controllers/RecipeController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MealPlanner.Data;
+using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MealPlanner.Controllers
 {
     public class RecipeController : Controller
     {
+        private readonly AppDbContext _db;
+        public RecipeController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Ingredient()
         {
-            return View();
+            List<Ingredient> objIngredientList = _db.Ingredients.ToList();
+            return View(objIngredientList);
         }
         public IActionResult Steps()
         {
