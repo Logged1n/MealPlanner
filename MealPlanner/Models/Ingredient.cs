ce40e33a20681da79741badb54e161705a27f57f
Łukasz Halicki <85924@student.pb.edu.pl>
2025-01-12 21:07:05 +0100

    dzieki narazie

--- MealPlanner/Models/Ingredient.cs
+++ MealPlanner/Models/Ingredient.cs
@@ -4,17 +4,8 @@ namespace MealPlanner.Models
 {
     public class Ingredient : MealComponent
     {
-        public string Unit { get; set; }
-        public override Enum Category
-        {
-            get => base.Category;
-            set
-            {
-                if (value is not IngredientCategory)
-                    throw new ArgumentException("Invalid enumeration type for this meal component.");
-                base.Category = value;
-            }
-        }
+        public string Unit { get; set; } = string.Empty;
+        public IngredientCategory Category { get; set; }
         
     }
 }
--- MealPlanner/Models/MealPlan.cs
+++ MealPlanner/Models/MealPlan.cs
@@ -4,16 +4,7 @@ namespace MealPlanner.Models
 {
     public class MealPlan : MealComponent
     {
-        public override Enum Category
-        {
-            get => base.Category;
-            set
-            {
-                if (value is not MealPlanDuration)
-                    throw new ArgumentException("Invalid enumeration type for this meal component.");
-                base.Category = value;
-            }
-        }
+        public MealPlanDuration Category { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public readonly List<MealDay> Components = [];
