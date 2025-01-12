c84c9dc7626c517202b41ba4e54174bd4f933a18
dawid <dawiddavtyan16@gmail.com>
2025-01-12 17:13:59 +0100

    Wstępny front

---
+++ MealPlanner/Controllers/PlanController.cs
@@ -0,0 +1,17 @@
+﻿using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.Controllers
+{
+    public class PlanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details() 
+        {
+            return View();
+        }
+    }
+}
---
+++ MealPlanner/Controllers/RecipeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ingredient()
+        {
+            return View();
+        }
+        public IActionResult Steps()
+        {
+            return View();
+        }
+    }
+}
