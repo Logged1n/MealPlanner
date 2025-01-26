251e63ec78c67981b61a5cbc5595a64e1fb75253
Patryk Zadykowicz <patrykzadykowiczali@gmail.com>
2025-01-27 00:48:38 +0100

    Dodanie połączenia z bazą dla meal day i meal plan

--- MealPlanner/Controllers/MealDayController.cs
+++ MealPlanner/Controllers/MealDayController.cs
@@ -1,13 +1,23 @@
-﻿using MealPlanner.Models;
+﻿using MealPlanner.Data;
+using MealPlanner.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MealPlanner.Controllers
 {
     public class MealDayController : Controller
     {
+
+        private readonly AppDbContext _db;
+
+        public MealDayController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Recipe> recipes = _db.Recipies.ToList();
+            return View(recipes);
         }
         public IActionResult InsertMealDay() 
         {
--- MealPlanner/Controllers/MealPlanController.cs
+++ MealPlanner/Controllers/MealPlanController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MealPlanner.Data;
+using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MealPlanner.Controllers
 {
     public class MealPlanController : Controller
     {
+
+        private readonly AppDbContext _db;
+
+        public MealPlanController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<MealPlan> mealPlans=_db.MealPlans.ToList();
+            return View(mealPlans);
         }
         public IActionResult InsertMealPlan()
         {
